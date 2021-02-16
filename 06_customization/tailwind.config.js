af943dc54efa7c5a7e7968c9839ad188766f1cfc
lucianobragaweb <lucianobragaweb@gmail.com>
2021-02-15 22:54:56 -0300

    Customizations

---
+++ 06_customization/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      black: '#010101',
+      red: '#A83246',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
