88eeda8c1a68b026dc376bdc6e14c7f7b96a8d80
lucianobragaweb <lucianobragaweb@gmail.com>
2021-02-16 13:21:51 -0300

    Tipography

---
+++ 09_tipography/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'arial': 'Arial'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
